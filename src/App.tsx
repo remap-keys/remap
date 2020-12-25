d49551a30a6fadac62b276e3b150d2e8af778a72
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-12-25 10:47:43 +0900

    Move the hid directory to src/services/hid.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Configure from './components/configure/Configure';
-import Hid from './hid/ui/Hid';
+import Hid from './services/hid/ui/Hid';
 import Top from './Top';
 
 const App = () => {
