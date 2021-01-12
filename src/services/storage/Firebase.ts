ef6cd6399bea610c18b381dbacf8c5eb0ef84f61
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-01-12 14:33:42 +0900

    Define environment variables for Firebase on env files.

--- src/services/storage/Firebase.ts
+++ src/services/storage/Firebase.ts
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import { IFetchKeyboardDefinitionResult, IStorage } from './Storage';
 
 const config = {
-  apiKey: 'AIzaSyDiEpH2Bbf1ZO63ZiJM3SeyD2LR0IeyM1s',
-  authDomain: 'remap-4f4d9.firebaseapp.com',
-  projectId: 'remap-4f4d9',
-  storageBucket: 'remap-4f4d9.appspot.com',
-  messagingSenderId: '981855678093',
-  appId: '1:981855678093:web:f293b8e994e6952377bcc4',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export class FirestoreStorage implements IStorage {
