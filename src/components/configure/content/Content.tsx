7ba265e88f4cf2e72e5909b1b9321079029776a7
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-01-28 07:05:51 +0900

    Fix linter isuue.

--- src/components/configure/content/Content.tsx
+++ src/components/configure/content/Content.tsx
@@ -6,7 +6,6 @@ import { ISetupPhase, SetupPhase } from '../../../store/state';
 import KeyboardDefinitionForm from '../keyboarddefform/KeyboardDefinitionForm.container';
 import Remap from '../remap/Remap.container';
 import { CircularProgress } from '@material-ui/core';
-import Footer from '../../common/footer/Footer.container';
 
 type ContentState = {};
 
