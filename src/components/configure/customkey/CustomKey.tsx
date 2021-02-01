f67c5d0c858d58dc991f642b7308033f0e10e569
adam <adamrocker@gmail.com>
2021-02-02 05:18:37 +0900

    save

--- src/components/configure/customkey/CustomKey.tsx
+++ src/components/configure/customkey/CustomKey.tsx
@@ -31,9 +31,18 @@ type OwnProps = {
 
 type OwnState = {
   value: KeycodeOption | null;
+  value2: KeycodeOption | null;
+  valueMT: KeycodeOption | null;
   inputValue: string;
+  inputValue2: string;
+  inputValueMT: string;
   label: string;
   code: string;
+  rl: number; // 0:left, 1:right
+  shift: boolean;
+  ctrl: boolean;
+  alt: boolean;
+  gui: boolean;
   selectedTabIndex: number;
 };
 
@@ -43,15 +52,25 @@ export default class CustomKey extends React.Component<OwnProps, OwnState> {
     super(props);
     this.state = {
       value: null,
+      value2: null,
+      valueMT: null,
       inputValue: '',
+      inputValue2: '',
+      inputValueMT: '',
       selectedTabIndex: 0,
       label: '',
       code: '0',
+      rl: 0,
+      shift: false,
+      ctrl: false,
+      alt: false,
+      gui: false,
     };
     this.arrowRef = React.createRef();
   }
 
   private onEnter() {
+    //TODO: initialize input status
     const value: KeycodeOption = {
       category: 'Basic',
       subcategory: '',
@@ -98,9 +117,18 @@ export default class CustomKey extends React.Component<OwnProps, OwnState> {
     this.setState({ value });
     if (value) {
       const label = value.label;
-      this.setState({ label: label, code: value.code.toString(16) });
+      const code = this.getFullCode({ keycode: value.code });
+      this.setState({ label: label, code: code });
       this.emitKeyChange({ value });
     }
+  }
+
+  private updateValue2(value2: KeycodeOption | null) {
+    this.setState({ value2 });
+  }
+
+  private updateValueMT(valueMT: KeycodeOption | null) {
+    this.setState({ valueMT });
   }
 
   private updateLabel(label: string) {
@@ -116,8 +144,101 @@ export default class CustomKey extends React.Component<OwnProps, OwnState> {
     }
   }
 
+  private getFullCode(args: {
+    ctrl?: boolean;
+    shift?: boolean;
+    alt?: boolean;
+    gui?: boolean;
+    keycode?: number;
+    rl?: number;
+  }): string {
+    let mod = 0b0000;
+    if (args.ctrl || (args.ctrl == undefined && this.state.ctrl)) {
+      mod |= 0b0001;
+    }
+
+    if (args.shift || (args.shift == undefined && this.state.shift)) {
+      mod |= 0b0010;
+    }
+
+    if (args.alt || (args.alt == undefined && this.state.alt)) {
+      mod |= 0b0100;
+    }
+
+    if (args.gui || (args.gui == undefined && this.state.gui)) {
+      mod |= 0b1000;
+    }
+
+    if (0 < mod) {
+      if (args.rl == undefined) {
+        if (this.state.rl == 1) {
+          mod |= 0b1_0000;
+        } else {
+          mod |= 0b0_0000;
+        }
+      } else {
+        if (args.rl == 1) {
+          console.log('right');
+          mod |= 0b1_0000;
+        } else {
+          console.log('left');
+          mod |= 0b0_0000;
+        }
+      }
+    }
+    let codeNum =
+      args.keycode == undefined ? this.state.value!.code : args.keycode;
+    console.log(`codeNum: ${codeNum}`);
+    console.log(`mode: ${mod << 8}`);
+    if (codeNum <= 0xff) {
+      codeNum = (mod << 8) | codeNum;
+    }
+    const code = Number(codeNum).toString(16).toUpperCase();
+    return code;
+  }
+
+  private updateCtrl(ctrl: boolean) {
+    const code = this.getFullCode({ ctrl });
+    console.log(code);
+    this.updateCode(code);
+    this.setState({ ctrl });
+  }
+
+  private updateShift(shift: boolean) {
+    const code = this.getFullCode({ shift });
+    this.updateCode(code);
+    this.setState({ shift });
+  }
+
+  private updateAlt(alt: boolean) {
+    const code = this.getFullCode({ alt });
+    this.updateCode(code);
+    this.setState({ alt });
+  }
+
+  private updateGui(gui: boolean) {
+    const code = this.getFullCode({ gui });
+    this.updateCode(code);
+    this.setState({ gui });
+  }
+
+  private updateRL(_rl: string) {
+    const rl = parseInt(_rl);
+    const code = this.getFullCode({ rl });
+    this.updateCode(code);
+    this.setState({ rl });
+  }
+
   private setInputValue(inputValue: string) {
     this.setState({ inputValue });
+  }
+
+  private setInputValue2(inputValue2: string) {
+    this.setState({ inputValue2 });
+  }
+
+  private setInputValueMT(inputValueMT: string) {
+    this.setState({ inputValueMT });
   }
 
   private selectTab(event: React.ChangeEvent<{}>, selectedTabIndex: number) {
@@ -184,12 +305,10 @@ export default class CustomKey extends React.Component<OwnProps, OwnState> {
               onInputChange={(event, newInputValue) => {
                 console.log('onInputChange');
                 console.log(newInputValue);
-                this.setInputValue(newInputValue.split(',')[0]);
+                this.setInputValue(newInputValue.split('::')[0]);
               }}
               getOptionLabel={(option) =>
-                `${option.label}, ${option.category}, ${option.subcategory}, ${
-                  option.name.long
-                }, ${option.name.short}, ${hexadecimal(option.code)}`
+                `${option.label}::${option.category}::${option.subcategory}`
               }
               renderOption={(option) => (
                 <div className="customkey-select-item">
@@ -221,15 +340,19 @@ export default class CustomKey extends React.Component<OwnProps, OwnState> {
               row
               aria-label="position"
               name="left-right"
-              defaultValue="left"
+              defaultValue={'0'}
+              value={'' + this.state.rl}
+              onChange={(e) => {
+                this.updateRL(e.target.value);
+              }}
             >
               <FormControlLabel
-                value="left"
+                value="0"
                 control={<Radio color="primary" />}
                 label="Left"
               />
               <FormControlLabel
-                value="right"
+                value="1"
                 control={<Radio color="primary" />}
                 label="Right"
               />
@@ -241,22 +364,54 @@ export default class CustomKey extends React.Component<OwnProps, OwnState> {
             >
               <FormControlLabel
                 value="shift"
-                control={<Checkbox color="primary" />}
+                control={
+                  <Checkbox
+                    color="primary"
+                    onChange={(e) => {
+                      this.updateShift(e.target.checked);
+                    }}
+                    checked={this.state.shift}
+                  />
+                }
                 label="Shift"
               />
               <FormControlLabel
                 value="control"
-                control={<Checkbox color="primary" />}
+                control={
+                  <Checkbox
+                    color="primary"
+                    onChange={(e) => {
+                      this.updateCtrl(e.target.checked);
+                    }}
+                    checked={this.state.ctrl}
+                  />
+                }
                 label="Ctrl"
               />
               <FormControlLabel
                 value="alt"
-                control={<Checkbox color="primary" />}
+                control={
+                  <Checkbox
+                    color="primary"
+                    onChange={(e) => {
+                      this.updateAlt(e.target.checked);
+                    }}
+                    checked={this.state.alt}
+                  />
+                }
                 label="Alt"
               />
               <FormControlLabel
                 value="gui"
-                control={<Checkbox color="primary" />}
+                control={
+                  <Checkbox
+                    color="primary"
+                    onChange={(e) => {
+                      this.updateGui(e.target.checked);
+                    }}
+                    checked={this.state.gui}
+                  />
+                }
                 label="Win/Cmd"
               />
             </FormGroup>
@@ -265,18 +420,105 @@ export default class CustomKey extends React.Component<OwnProps, OwnState> {
             <div className="customkey-description">
               Hold to function [Modifiers], tap to send [Keycode].
             </div>
-            <TextField
-              variant="outlined"
-              label="Modifiers"
-              className="customkey-field customkey-label"
+            <Autocomplete
+              id="customkey-autocomplete"
+              className="customkey-field"
+              autoHighlight
+              freeSolo
               size="small"
+              options={dualFunctionalKeycodeOptions as KeycodeOption[]}
+              value={this.state.value2}
+              onChange={(
+                event: any,
+                newValue: string | KeycodeOption | null
+              ) => {
+                this.updateValueMT(newValue as KeycodeOption);
+              }}
+              inputValue={this.state.inputValue2}
+              onInputChange={(event, newInputValue) => {
+                this.setInputValueMT(newInputValue.split('::')[0]);
+              }}
+              getOptionLabel={(option) =>
+                `${option.label}::${option.category}::${option.subcategory}`
+              }
+              renderOption={(option) => (
+                <div className="customkey-select-item">
+                  <div className="keycode-label-wrapper">
+                    <div className="keycode-label">{option.label}</div>
+                    <div className="keycode-category">
+                      {option.category}
+                      {option.subcategory && ` / ${option.subcategory}`}
+                    </div>
+                  </div>
+                  {option.desc && (
+                    <div className="keycode-desc">{option.desc}</div>
+                  )}
+                </div>
+              )}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Hold to function modifiers"
+                  variant="outlined"
+                  inputProps={{
+                    ...params.inputProps,
+                  }}
+                />
+              )}
             />
-
-            <TextField
-              variant="outlined"
-              label="Keycode"
-              className="customkey-field customkey-label"
+            <Autocomplete
+              id="customkey-autocomplete"
+              className="customkey-field"
+              autoHighlight
+              freeSolo
               size="small"
+              options={
+                keycodeOptions.filter(
+                  (op) => op.category == 'Basic'
+                ) as KeycodeOption[]
+              }
+              value={this.state.value2}
+              onChange={(
+                event: any,
+                newValue: string | KeycodeOption | null
+              ) => {
+                console.log('onChange2');
+                console.log(newValue);
+                this.updateValue2(newValue as KeycodeOption);
+              }}
+              inputValue={this.state.inputValue2}
+              onInputChange={(event, newInputValue) => {
+                console.log('onInputChange2');
+                console.log(newInputValue);
+                this.setInputValue2(newInputValue.split('::')[0]);
+              }}
+              getOptionLabel={(option) =>
+                `${option.label}::${option.category}::${option.subcategory}`
+              }
+              renderOption={(option) => (
+                <div className="customkey-select-item">
+                  <div className="keycode-label-wrapper">
+                    <div className="keycode-label">{option.label}</div>
+                    <div className="keycode-category">
+                      {option.category}
+                      {option.subcategory && ` / ${option.subcategory}`}
+                    </div>
+                  </div>
+                  {option.desc && (
+                    <div className="keycode-desc">{option.desc}</div>
+                  )}
+                </div>
+              )}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Tap to send keycode"
+                  variant="outlined"
+                  inputProps={{
+                    ...params.inputProps,
+                  }}
+                />
+              )}
             />
           </TabPanel>
           <TabPanel value={this.state.selectedTabIndex} index={2}>
@@ -308,6 +550,11 @@ export default class CustomKey extends React.Component<OwnProps, OwnState> {
               }}
               value={this.state.code}
             />
+            <div className="customkey-bcode">
+              {('0000000000000000' + parseInt(this.state.code, 16).toString(2))
+                .slice(-16)
+                .replace(/([0-1]{4}?)/g, '$1 ')}
+            </div>
           </TabPanel>
         </div>
       </Popover>
@@ -416,8 +663,8 @@ const keycodeOptions: KeycodeOption[] = [
     code: 225,
     name: { long: 'KC_LSHIFT', short: 'KC_LSFT' },
     label: 'Left Shift',
-    category: 'Basic',
-    subcategory: 'Modifier',
+    category: 'Modifier',
+    subcategory: '',
   },
   {
     code: 135,
@@ -433,5 +680,40 @@ const keycodeOptions: KeycodeOption[] = [
     category: 'Special',
     subcategory: 'Firmware',
     desc: 'Resets the keyboard back to the bootloader, to flash new firmware.',
+  },
+];
+
+const dualFunctionalKeycodeOptions: KeycodeOption[] = [
+  {
+    code: 0b0110_0001,
+    name: { long: 'MT(CTRL|kc)', short: 'MT(CTRL|kc)' },
+    label: 'Ctrl',
+    category: '2 Functions',
+    subcategory: '',
+    desc: '',
+  },
+  {
+    code: 0b0110_0010,
+    name: { long: 'MT(SHIFT|kc)', short: 'MT(SHIFT|kc)' },
+    label: 'Shift',
+    category: '2 Functions',
+    subcategory: '',
+    desc: '',
+  },
+  {
+    code: 0b0110_0100,
+    name: { long: 'MT(ALT|kc)', short: 'MT(ALT|kc)' },
+    label: 'Alt',
+    category: '2 Functions',
+    subcategory: '',
+    desc: '',
+  },
+  {
+    code: 0b0110_1000,
+    name: { long: 'MT(GUI|kc)', short: 'MT(GUI|kc)' },
+    label: 'Win/Cmd',
+    category: '2 Functions',
+    subcategory: '',
+    desc: '',
   },
 ];
