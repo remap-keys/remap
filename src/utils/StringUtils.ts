e5c3c5f93320e8718dcf8cf1395b55de7ba3e382
adam <adamrocker@gmail.com>
2021-02-24 19:54:38 +0900

    applyed a japanese font and splited too long label to multi-lined label

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -2,3 +2,8 @@ export function hexadecimal(num: number, digit = 0): string {
   const hex = Array(digit).fill('0').join('') + new Number(num).toString(16);
   return `0x${hex.slice(-digit).toUpperCase()}`;
 }
+
+export function isDoubleWidthString(str: string): boolean {
+  // eslint-disable-next-line no-control-regex
+  return Boolean(str.match(/^[^\x01-\x7E\xA1-\xDF]+$/));
+}
