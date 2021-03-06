1b846525c3f5775e65b8ab6fc9648db067519ae5
adamrocker <masahiko.adachi@kabuku.co.jp>
2021-03-06 10:20:22 +0900

    replaced the info icon to the original rounded icon

---
+++ src/components/common/icons/InfoIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+export function InfoIcon(props: any) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M 11 7 h 2 v 2 h -2 z m 0 5 C 11 11 12 11 12 11 c 0 0 1 0 1 1 v 4 C 13 17 12 17 12 17 c 0 0 -1 0 -1 -1 z m 1 -10 C 6.48 2 2 6.48 2 12 s 4.48 10 10 10 s 10 -4.48 10 -10 S 17.52 2 12 2 z m 0 18 c -4.41 0 -8 -3.59 -8 -8 s 3.59 -8 8 -8 s 8 3.59 8 8 s -3.59 8 -8 8 z" />
+    </SvgIcon>
+  );
+}
