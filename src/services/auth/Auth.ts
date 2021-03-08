3c17d1634a21b9d1f062881f74434bb49618ad9a
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-03-08 10:18:01 +0900

    Refactor the logic to get a GitHub provider data.

--- src/services/auth/Auth.ts
+++ src/services/auth/Auth.ts
@@ -2,7 +2,32 @@ import firebase from 'firebase';
 
 export interface IAuth {
   signInWithGitHub(): Promise<void>;
+  // eslint-disable-next-line no-unused-vars
   subscribeAuthStatus(callback: (user: firebase.User | null) => void): void;
   getCurrentAuthenticatedUser(): firebase.User;
   signOutFromGitHub(): Promise<void>;
 }
+
+export type IGetProviderDataResult = {
+  exists: boolean;
+  userInfo?: firebase.UserInfo;
+};
+
+export const getGitHubProviderData = (
+  user: firebase.User
+): IGetProviderDataResult => {
+  const providerData = user.providerData;
+  const githubProviderData = providerData.find(
+    (data) => data?.providerId === 'github.com'
+  );
+  if (githubProviderData) {
+    return {
+      exists: true,
+      userInfo: githubProviderData,
+    };
+  } else {
+    return {
+      exists: false,
+    };
+  }
+};
