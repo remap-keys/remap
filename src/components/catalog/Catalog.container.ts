de397e75745cbfeb745c29aeedb6e209cf1d757b
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-07-07 15:14:28 +0900

    Support specifying search conditions on a query parameter.

--- src/components/catalog/Catalog.container.ts
+++ src/components/catalog/Catalog.container.ts
@@ -1,12 +1,34 @@
 import { connect } from 'react-redux';
 import Catalog from './Catalog';
-import { ICatalogPhase, RootState } from '../../store/state';
+import {
+  ALL_HOTSWAP_TYPE,
+  ALL_KEY_SWITCH_TYPE,
+  ALL_LED_TYPE,
+  ALL_MCU_TYPE,
+  ALL_OLED_TYPE,
+  ALL_SPEAKER_TYPE,
+  ALL_SPLIT_TYPE,
+  ALL_STAGGERED_TYPE,
+  ICatalogPhase,
+  IKeyboardFeatures,
+  IKeyboardHotswapType,
+  IKeyboardKeySwitchType,
+  IKeyboardLedType,
+  IKeyboardMcuType,
+  IKeyboardOledType,
+  IKeyboardSpeakerType,
+  IKeyboardSplitType,
+  IKeyboardStaggeredType,
+  RootState,
+} from '../../store/state';
 import { AppActions, NotificationActions } from '../../actions/actions';
 import {
   catalogActionsThunk,
   CatalogAppActions,
+  CatalogSearchActions,
 } from '../../actions/catalog.action';
 import { storageActionsThunk } from '../../actions/storage.action';
+import { ParsedQs } from 'qs';
 
 // eslint-disable-next-line no-unused-vars
 const mapStateToProps = (state: RootState) => {
@@ -40,6 +62,79 @@ const mapDispatchToProps = (_dispatch: any) => {
     },
     updateSignedIn: (signedIn: boolean) => {
       _dispatch(AppActions.updateSignedIn(signedIn));
+    },
+    updateSearchCondition: (params: ParsedQs) => {
+      if (params.keyword) {
+        _dispatch(CatalogSearchActions.updateKeyword(params.keyword as string));
+      }
+      if (params.features) {
+        (params.features as string).split(',').forEach((feature: string) => {
+          if (ALL_SPLIT_TYPE.includes(feature as IKeyboardSplitType)) {
+            _dispatch(
+              CatalogSearchActions.updateFeatures(
+                feature as IKeyboardFeatures,
+                ALL_SPLIT_TYPE
+              )
+            );
+          }
+          if (ALL_STAGGERED_TYPE.includes(feature as IKeyboardStaggeredType)) {
+            _dispatch(
+              CatalogSearchActions.updateFeatures(
+                feature as IKeyboardFeatures,
+                ALL_STAGGERED_TYPE
+              )
+            );
+          }
+          if (ALL_LED_TYPE.includes(feature as IKeyboardLedType)) {
+            _dispatch(
+              CatalogSearchActions.updateFeatures(
+                feature as IKeyboardFeatures,
+                ALL_LED_TYPE
+              )
+            );
+          }
+          if (ALL_KEY_SWITCH_TYPE.includes(feature as IKeyboardKeySwitchType)) {
+            _dispatch(
+              CatalogSearchActions.updateFeatures(
+                feature as IKeyboardFeatures,
+                ALL_KEY_SWITCH_TYPE
+              )
+            );
+          }
+          if (ALL_HOTSWAP_TYPE.includes(feature as IKeyboardHotswapType)) {
+            _dispatch(
+              CatalogSearchActions.updateFeatures(
+                feature as IKeyboardFeatures,
+                ALL_HOTSWAP_TYPE
+              )
+            );
+          }
+          if (ALL_MCU_TYPE.includes(feature as IKeyboardMcuType)) {
+            _dispatch(
+              CatalogSearchActions.updateFeatures(
+                feature as IKeyboardFeatures,
+                ALL_MCU_TYPE
+              )
+            );
+          }
+          if (ALL_OLED_TYPE.includes(feature as IKeyboardOledType)) {
+            _dispatch(
+              CatalogSearchActions.updateFeatures(
+                feature as IKeyboardFeatures,
+                ALL_OLED_TYPE
+              )
+            );
+          }
+          if (ALL_SPEAKER_TYPE.includes(feature as IKeyboardSpeakerType)) {
+            _dispatch(
+              CatalogSearchActions.updateFeatures(
+                feature as IKeyboardFeatures,
+                ALL_SPEAKER_TYPE
+              )
+            );
+          }
+        });
+      }
     },
   };
 };
