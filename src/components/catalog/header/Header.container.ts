df4ff4bf1443d97dae892881e66bd03e38956bbc
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-07-26 14:12:16 +0900

    Apply a new style on the header.

--- src/components/catalog/header/Header.container.ts
+++ src/components/catalog/header/Header.container.ts
@@ -1,14 +1,20 @@
 import { connect } from 'react-redux';
 import { RootState } from '../../../store/state';
 import Header from './Header';
-import { catalogActionsThunk } from '../../../actions/catalog.action';
+import {
+  catalogActionsThunk,
+  CatalogAppActions,
+} from '../../../actions/catalog.action';
 import { AppActionsThunk } from '../../../actions/actions';
+import { storageActionsThunk } from '../../../actions/storage.action';
 
 // eslint-disable-next-line no-unused-vars
 const mapStateToProps = (state: RootState) => {
   return {
     auth: state.auth.instance,
     signedIn: state.app.signedIn,
+    phase: state.catalog.app.phase,
+    definitionDocument: state.entities.keyboardDefinitionDocument,
   };
 };
 export type HeaderStateType = ReturnType<typeof mapStateToProps>;
@@ -24,6 +30,15 @@ const mapDispatchToProps = (_dispatch: any) => {
     },
     linkToGitHubAccount: () => {
       _dispatch(AppActionsThunk.linkToGitHubAccount());
+    },
+    goToSearch: () => {
+      _dispatch(storageActionsThunk.searchKeyboardsForCatalog());
+    },
+    goToKeymap: () => {
+      _dispatch(CatalogAppActions.updatePhase('keymap'));
+    },
+    goToIntroduction: () => {
+      _dispatch(CatalogAppActions.updatePhase('introduction'));
     },
   };
 };
