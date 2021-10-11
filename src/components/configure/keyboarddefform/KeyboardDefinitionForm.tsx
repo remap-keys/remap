2e9808e8b9938e8232e99f53e912282aff34aab7
adam <adamrocker@gmail.com>
2021-10-11 12:11:11 +0900

    fixed noreferrer string

--- src/components/configure/keyboarddefform/KeyboardDefinitionForm.tsx
+++ src/components/configure/keyboarddefform/KeyboardDefinitionForm.tsx
@@ -55,7 +55,7 @@ export default class KeyboardDefinitionForm extends React.Component<
         <KeyboardDefinitionFormPart
           messageHtml={`Please import your <strong>${
             this.props.keyboardInfo!.productName
-          }</strong> definition file(<a href="https://caniusevia.com/docs/specification/" target="_blank" rel="referrer">.json</a>).`}
+          }</strong> definition file(<a href="https://caniusevia.com/docs/specification/" target="_blank" rel="noreferrer">.json</a>).`}
           validateDeviceIds={true}
           deviceVendorId={this.props.keyboardInfo!.vendorId}
           deviceProductId={this.props.keyboardInfo!.productId}
