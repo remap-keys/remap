71885375fd8727eab24335fe1629b3c9a07886b3
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-05-25 15:36:51 +0900

    Migrating from .eslintrc to Flatconfig

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+// @ts-check
+/** @type {import("eslint").Linter.FlatConfig[]} */
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import typeScriptESLintParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      react,
+    },
+    languageOptions: {
+      parser: typeScriptESLintParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+);
