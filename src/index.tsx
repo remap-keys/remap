fa33cf44cafa0d679d34ed92cf1341ddb67f75d3
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-05-25 19:57:00 +0900

    remove reportWebVitals.ts and web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import reducers from './store/reducers';
-import reportWebVitals from './reportWebVitals';
 import { errorReportingLogger } from './utils/ErrorReportingLogger';
 import OGP from './components/common/ogp/OGP.container';
 import { HelmetProvider } from 'react-helmet-async';
@@ -37,8 +36,3 @@ ReactDOM.render(
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
