3bdd7721b14df09394fca1529a471f5bc58d1a5f
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-05-26 23:11:18 +0900

    @vitejs/plugin-react to @vitejs/plugin-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from "node:path";
 import { readFileSync, existsSync } from "node:fs";
 import { defineConfig, loadEnv, Plugin, createFilter, transformWithEsbuild } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 
