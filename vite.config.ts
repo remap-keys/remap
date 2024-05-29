26338e7dbb745cce383c69d181eb761c794772e3
Yoichiro Tanaka <yoichiro6642@gmail.com>
2024-05-29 17:45:54 +0900

    Temporary commit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin, loadEnv } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react';`,
-  },
+  plugins: [react(), envPlugin()],
+  esbuild: {},
   build: {
     sourcemap: true,
   },
-  root: 'src',
   server: {
     port: 3000,
   },
 });
+
+function envPlugin(): Plugin {
+  return {
+    name: 'env-plugin',
+    config(_, { mode }) {
+      const env = loadEnv(mode, '.', ['REACT_APP_', 'NODE_ENV']);
+      return {
+        define: Object.fromEntries(
+          Object.entries(env).map(([key, value]) => [
+            `import.meta.env.${key}`,
+            JSON.stringify(value),
+          ])
+        ),
+      };
+    },
+  };
+}
